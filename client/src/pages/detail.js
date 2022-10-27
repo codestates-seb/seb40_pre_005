3843122cadd558047fd0e9206c335dc561358a89
Ko-Eunseo <koyk0408@naver.com>
2022-10-27 13:28:10 +0900

    make detail page and branch

---
+++ client/src/pages/detail.js
@@ -0,0 +1,17 @@
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Nav from '../components/Nav';
+import Sidebar from '../components/Sidebar';
+
+const detail = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <Sidebar />
+      <Footer />
+    </>
+  );
+};
+
+export default detail;
