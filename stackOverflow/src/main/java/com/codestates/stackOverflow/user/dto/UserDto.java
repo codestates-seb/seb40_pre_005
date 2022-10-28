169a860ff9979cb0a54273f508e79167e8295acf
JunYeol <wnsduf9160@naver.com>
2022-10-28 11:33:55 +0900

    password 조건 추가

--- stackOverflow/src/main/java/com/codestates/stackOverflow/user/dto/UserDto.java
+++ stackOverflow/src/main/java/com/codestates/stackOverflow/user/dto/UserDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 public class UserDto {
 
@@ -18,6 +19,7 @@ public class UserDto {
         private String name;
 
         @NotNull
+        @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
         private String userPassword;
 
     }
