369822a2339159e117e35fbbb7d288408796905e
KANG-MoonSoo <munsu2483@gmail.com>
2022-11-01 11:09:30 +0900

    조회수 구현

--- src/main/java/com/codestates/stackOverflow/exception/ExceptionCode.java
+++ src/main/java/com/codestates/stackOverflow/exception/ExceptionCode.java
@@ -5,8 +5,11 @@ import lombok.Getter;
 public enum ExceptionCode {
     user_NOT_FOUND(404, "User not found"),
     user_EXISTS(409, "User exists"),
+    QUESTION_NOT_FOUND(404, "Question not found"), // Question 추가
+
     NOT_IMPLEMENTATION(501, "Not Implementation"),
     INVALID_user_STATUS(400, "Invalid User status");
+
 
     @Getter
     private int status;
