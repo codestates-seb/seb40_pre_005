e95056656c6074161109bb93bb618e18ec1c9908
Ko-Eunseo <koyk0408@naver.com>
2022-11-03 11:00:33 +0900

    무한로딩의 굴레에서 벗어나고프다...

---
+++ client/src/components/AnswerItem.js
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import Writer from '../components/Writer';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const Answer = styled.div`
+  padding-bottom: 24px;
+  border-bottom: 1px solid #d6d9dc;
+  .handleBtns {
+    button {
+      border: none;
+      margin: 4px;
+      color: #6a737c;
+      font-size: 13px;
+      cursor: pointer;
+    }
+  }
+`;
+const AnswerItem = ({ answer }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [editText, setEditText] = useState();
+  console.log(answer);
+  //DELETE
+  const handleDelete = () => {
+    const url = `http://localhost:3001/answer?id=${answer?.answerId}`;
+    const fetchData = async () => {
+      try {
+        await axios.delete(url).then(() => {
+          window.location.reload();
+        });
+      } catch (err) {
+        console.log('error', err);
+      }
+    };
+    fetchData();
+  };
+  // UPDATE
+  const handleEditBtn = () => {
+    setIsEdit(!isEdit);
+  };
+  // const onChange = (e) => {
+  //   setEditText(e.target.value);
+  // };
+  // useEffect(() => {
+  //   // const url = REACT_APP_ANSWER + ${answerId};
+  //   const url = `https://localhost:3001/answer?id=${answerId}`;
+  //   const data = {
+  //     answerId,
+  //     answerStatus: 'ANSWER_NOT_EXIST',
+  //     body: editText,
+  //   };
+  //   const fetchData = async () => {
+  //     try {
+  //       await axios.patch(url, data).then((res) => {
+  //         console.log(res);
+  //       });
+  //     } catch (err) {
+  //       console.log('error', err);
+  //     }
+  //   };
+  //   fetchData();
+  // }, [isEdit]);
+  return (
+    <Answer>
+      <AnswerItem>
+        <div>
+          <h2>Answers</h2>
+          <div>{answer?.body}</div>
+        </div>
+        <Writer props={answer?.userId} />
+        <div className="handleBtns">
+          {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+          <button onClick={handleEditBtn}>edit</button>
+          {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+          <button onClick={handleDelete}>delete</button>
+        </div>
+      </AnswerItem>
+    </Answer>
+  );
+};
+
+export default AnswerItem;
