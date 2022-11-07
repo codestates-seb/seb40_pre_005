23c93fcdd2a491cde0acde8384a04112ef57d7b7
JunYeol <wnsduf9160@naver.com>
2022-11-07 18:03:30 +0900

    debug false

--- stackOverflow/src/main/java/com/codestates/stackOverflow/config/SecurityConfiguration.java
+++ stackOverflow/src/main/java/com/codestates/stackOverflow/config/SecurityConfiguration.java
@@ -12,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity(debug = false)
 @Configuration
 public class SecurityConfiguration {
 
