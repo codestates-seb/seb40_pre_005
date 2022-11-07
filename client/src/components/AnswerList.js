d47d4b6e9b7a894766320f2faea03e062f677e8a
Ko-Eunseo <koyk0408@naver.com>
2022-11-07 10:12:57 +0900

    필요없는 코드 삭제

--- client/src/components/AnswerList.js
+++ client/src/components/AnswerList.js
@@ -8,50 +8,13 @@ const AnswersWrapper = styled.div`
   padding: 24px 0;
 `;
 
-const AnswerList = ({ questionId }) => {
-  const [answers, setAnswers] = useState([]);
-  const [pageInfo, setPageInfo] = useState();
-  const [selectedPage, setSelectedPage] = useState(1);
-  const [pageSize, setPageSize] = useState(5);
-
-  //answer전체조회
-  async function getAnswerList({ page, size }) {
-    const res = await axios.get(
-      `${process.env.REACT_APP_ANSWER}?page=${page}&size=${size}`
-    );
-    return res.data;
-  }
+const AnswerList = ({ questionId, questionInfo }) => {
+  const [answers, setAnswers] = useState(null);
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await getAnswerList({
-        page: selectedPage,
-        size: pageSize,
-      });
-
-      setPageInfo(data.pageInfo);
-      setAnswers(data.data);
-    };
-
-    fetchData();
-  }, [selectedPage, pageSize]);
-  console.log(answers);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     // const url = `http://localhost:3001/answer?questionId=${questionId}`;
-  //     // const url = `${process.env.REACT_APP_ANSWER}/${answerId}`;
-  //     try {
-  //       await axios.get(url).then((res) => {
-  //         setAnswers(res.data);
-  //         setPageInfo(res.pageInfo);
-  //       });
-  //     } catch (err) {
-  //       console.log('error', err);
-  //     }
-  //   };
-  //   fetchData();
-  // }, [selectedPage, pageSize]);
-
+    if (questionInfo) {
+      setAnswers(questionInfo.answers?.data);
+    }
+  }, [questionInfo]);
   return (
     <AnswersWrapper>
       {answers
